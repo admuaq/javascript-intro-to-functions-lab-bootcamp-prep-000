3d52635134bedb257ae8e4e4a3b7faea3ce519bb
Adham Muhammad <admuaq@gmail.com>
2018-06-18 10:41:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   return console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string.toUpperCase) {
+function sayHiToGrandma(string) {
   return 'I can\'t hear you!';
 }
