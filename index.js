310af2ca9c308174a7039ade1a315894b1352cd3
Adham Muhammad <admuaq@gmail.com>
2018-06-18 10:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if (string.toLowerCase) {
+  if (string.toLowerCase()) {
   return "I can\'t hear you!";
-  } else if (string.toUpperCase) {
+  } 
+  else if (string.toUpperCase()) {
   return "YES INDEED!";
   }
 }
